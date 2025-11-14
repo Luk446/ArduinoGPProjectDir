520835a9d3eb9a8f6d5cdadc930cd58cf69a31d7
Luke Edgecombe <145694364+Luk446@users.noreply.github.com>
2025-11-14 18:58:26 +0000

    feat(reformat) move around files

--- Arduino/EspNowChek/EspNowChek.ino
+++ Arduino/EspNowChek/EspNowChek.ino
@@ -4,11 +4,11 @@
 
 
 void setup() {
-  Serial.begin(115200);
+  Serial0.begin(115200);
   WiFi.mode(WIFI_STA);
   // add delay for wifi
   delay(100);
-  Serial.println(WiFi.macAddress());
+  Serial0.println(WiFi.macAddress());
 
 }
 
